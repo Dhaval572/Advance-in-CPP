18482d1c697d7cf6b336f06f7904c9c3e35008b5
Dhaval <dp9718776@gmail.com>
2025-03-02 21:56:47 +0530

    Create Compile_time_if_else.cpp

---
+++ Compile_time_if_else.cpp
@@ -0,0 +1,38 @@
+// Compile time if else
+#include <iostream>
+using namespace std;
+
+#ifdef _WIN32
+    #define PLATFORM "Windows"
+#elif __linux__
+    #define PLATFORM "Linux"
+#elif __APPLE__
+    #define PLATFORM "MacOS"
+#else
+    #define PLATFORM "Unknown OS"
+#endif
+
+int main()
+{
+    cout << "Running on " << PLATFORM ;
+    return 0;
+}
+/*
+Explanation of #ifdef, #else, #elif, #endif
+
+#ifdef:
+
+    Checks the conditions like if keyword.
+
+#else:
+
+    Default condition like else keyword.
+
+#elif:
+
+    Checks the next contition like else if.
+
+#endif:
+
+    Used to end the #ifdef.
+*/
